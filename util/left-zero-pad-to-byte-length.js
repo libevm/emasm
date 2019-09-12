3a076d25a4820641e9d8d19a363a2b4e27724151
Raymond Pulver IV <raymond.pulver_iv@uconn.edu>
2019-09-12 23:47:49 +0000

    fix

--- util/left-zero-pad-to-byte-length.js
+++ util/left-zero-pad-to-byte-length.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const leftZeroPadToByteLength = (s, n) => {
-	console.log(s);
 	if (typeof s === 'number') s = s.toString(16);
-	console.log([s, n]);
 	return Array(n*2 - s.length + 1).join('0') + s;
 };
 
