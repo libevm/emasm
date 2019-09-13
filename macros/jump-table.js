b2b3823c9ac2de698467438c7090b24d69e3193a
Raymond Pulver IV <raymond.pulver_iv@uconn.edu>
2019-09-13 01:37:58 +0000

    update

--- macros/jump-table.js
+++ macros/jump-table.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const createJumpTable = (jumpTableLabel, labels) => [ 'bytes:' + jumpTableLabel, labels.map((label) => [ 2, label ]) ];
+const createJumpTable = (jumpTableLabel, labels, size = 32) => [ 'bytes:' + jumpTableLabel, labels.map((label) => [ size, label ]) ];
 
 module.exports = createJumpTable;
