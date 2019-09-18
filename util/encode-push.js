3c578733d801905582df6ace3fcb9daab4d61bf2
Raymond Pulver IV <raymond.pulver_iv@uconn.edu>
2019-09-18 11:32:22 +0000

    update

--- util/encode-push.js
+++ util/encode-push.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const addHexPrefix = require('./add-hex-prefix');
+const leftZeroPadToByteLength = require('./left-zero-pad-to-byte-length');
+const ops = require('../lib/ops');
+
 const encodePush = (v, length) => {
   const baseOffset = Number(addHexPrefix(ops.push));
   const opcodeValue = baseOffset + length - 1;
